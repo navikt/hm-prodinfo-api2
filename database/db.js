8e8fad62cfbc541947a507b805c16ba05189daf2
arshadkamran <arshad.ali.kamran@nav.no>
2021-06-05 00:28:59 +0200

    oppdatering av access policy og kode biblote versjoner

--- database/db.js
+++ database/db.js
@@ -1,8 +1,8 @@
 //require('dotenv').config();
-const sql = require("mssql/tedious")
+var sql = require("mssql/tedious")
 const fetch = require("node-fetch")
 
-const config = {
+var config = {
   server: process.env.DB_HOSTNAME,
   authentication: {
     type: "ntlm",
@@ -23,44 +23,13 @@ const config = {
 const poolPromise = connectDB()
 
 async function connectDB() {
-  console.log("Waiting for istio proxy")
-
-  await waitForIstioProxy()
-  console.log("Istio proxy should be ready")
-
   return new sql.ConnectionPool(config)
-    .connect()
-    .then((pool) => {
-      console.log("Connected to MSSQL")
-      return pool
-    })
-    .catch((err) => console.log("Database Connection Failed! Bad Config: ", err))
-}
-
-async function waitForIstioProxy() {
-  const errors = []
-  for (let i = 0; i < 30; i++) {
-    console.log("Waiting " + i)
-
-    try {
-      const result = await fetch("http://127.0.0.1:15000/ready")
-      if (result.ok) {
-        console.log("Got ok from istio proxy")
-
-        return
-      }
-    } catch (error) {
-      errors.push(error)
-    }
-
-    await sleep(1000)
-  }
-
-  throw new Error(errors)
-}
-
-function sleep(duration) {
-  return new Promise((resolve) => setTimeout(resolve, duration))
+      .connect()
+      .then((pool) => {
+        console.log("Connected to MSSQL")
+        return pool
+      })
+      .catch((err) => console.log("Database Connection Failed! Bad Config: ", err))
 }
 
 module.exports = {
